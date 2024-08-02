3dcf302d59355642fd4cf6e52b7611aa69c7dd88
SaiRishab <78468475+arikaki@users.noreply.github.com>
2024-08-02 16:33:53 -0400

    updates

--- backstage-demo/packages/app/src/App.tsx
+++ backstage-demo/packages/app/src/App.tsx
@@ -36,6 +36,7 @@ import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
 import { catalogEntityCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import { githubAuthApiRef } from '@backstage/core-plugin-api';
 
 const app = createApp({
   apis,
